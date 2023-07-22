b4a840b21555d559d937b9f3daf0049f6c0bb38b
Vasyl Yefimenko <rednez@gmail.com>
2023-07-22 17:19:05 +0200

    fix(front): handle the id token expired error
    Refreshing the token on catch 'Firebase token has been expired' error.

--- apps/front/src/app/graphql.module.ts
+++ apps/front/src/app/graphql.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { Observable } from '@apollo/client/core';
 import { ApolloLink, InMemoryCache } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
 import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from '../environments/environment';
+import firebase from 'firebase/compat/app';
 
 function createApollo(httpLink: HttpLink) {
   const uri = environment.apollo.uri;
@@ -29,9 +32,44 @@ function createApollo(httpLink: HttpLink) {
     }
   });
 
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+      if (graphQLErrors[0].message.includes('Firebase ID token has expired')) {
+        return new Observable((observer) => {
+          firebase
+            .auth()
+            .currentUser?.getIdToken(true)
+            .then((idToken) => {
+              sessionStorage.setItem('accessToken', idToken);
+
+              operation.setContext(({ headers = {} }) => {
+                return {
+                  headers: {
+                    ...headers,
+                    Authorization: 'Bearer ' + idToken,
+                  },
+                };
+              });
+
+              const subscriber = {
+                next: observer.next.bind(observer),
+                error: observer.error.bind(observer),
+                complete: observer.complete.bind(observer),
+              };
+
+              forward(operation).subscribe(subscriber);
+            })
+            .catch((error) => {
+              observer.error(error);
+            });
+        });
+      }
+    }
+  });
+
   return {
     cache: new InMemoryCache(),
-    link: ApolloLink.from([basic, auth, httpLink.create({ uri })]),
+    link: ApolloLink.from([basic, auth, errorLink, httpLink.create({ uri })]),
   };
 }
 
